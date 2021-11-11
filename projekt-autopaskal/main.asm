f8aab44f729078123c6516e9605153582f57aa5c
Lovro Govekar <lovro.govekar@gmail.com>
2021-11-11 16:25:21 +0100

    proof_of_concept-LED

--- projekt-autopaskal/main.asm
+++ projekt-autopaskal/main.asm
@@ -6,7 +6,34 @@
 ;
 
 
-; Replace with your application code
+	.org 	0x00
+	
+    
+
 start:
-    inc r16
-    rjmp start
+	call setup
+	call turn_on_4
+
+	rjmp loop
+
+setup:
+	ldi	r16,(1<<PINB4)		; load 00000001 into register 16
+    out     DDRB,r16		; write register 16 to DDRB
+
+	ret
+
+turn_on_4:
+	ldi	r16,(1<<PINB4)		; load 00000001 into register 16
+	out     PORTB,r16		; write register 16 to PORTB
+	ret
+
+turn_off_4:
+	ldi	r16,(0<<PINB4)		; load xxxxxxx0 into register 16
+	out PORTB, r16
+	ret
+	
+	
+
+loop:	rjmp    loop			; stay in infinite loop
+
+
