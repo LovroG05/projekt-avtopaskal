cd638d2945195cfa9289cada2346fb5eaa8f1c9e
Lovro Govekar <lovro.govekar@gmail.com>
2021-11-13 21:12:03 +0100

    rečmo da gumb

--- projekt-autopaskal/main.asm
+++ projekt-autopaskal/main.asm
@@ -2,10 +2,12 @@
 ; projekt-autopaskal.asm
 ;
 ; Created: 11. 11. 2021 14:30:11
-; Author : Lovro
+; Author : Lovro Govekar
 ;
 
 .def led_state = r22
+.def button_flag = r23
+.def last_button_flag = r21
 
 
 .cseg
@@ -24,7 +26,7 @@ start:
 setup:
 	call setupUART
 	;clr EICRA
-	ldi r16, (1<<ISC01)|(1<<ISC00)	
+	ldi r16, (1<<ISC01)|(0<<ISC00)	
 	sts EICRA, r16
 	ldi r16, (1<<INT0)				
 	out EIMSK, r16					
@@ -41,12 +43,14 @@ setup:
     out     DDRB,r16		; write register 16 to DDRB
     ; out     PORTB,r16	
 
-	clr	r16;,(1<<PIND2)		; load 00000001 into register 16
+	clr	r16		; load 00000001 into register 16
     out DDRD,r16		; write register 16 to DDRB
 	ldi r16,(1<<PIND2)
 	out PORTD, r16
 
 	ldi led_state, 0
+	ldi button_flag, 0
+	mov last_button_flag, button_flag
 	;call turn_on_4
 
 	rjmp loop
@@ -54,26 +58,39 @@ setup:
 turn_on_4:
 	ldi	r16,(1<<PINB4)		; load 00000001 into register 16
 	out PORTB,r16		; write register 16 to PORTB
-	ldi led_state, 1
-	reti
+	ldi button_flag, 1
+	ret
 
 turn_off_4:
 	ldi	r16,(0<<PINB4)		; load xxxxxxx0 into register 16
 	out PORTB, r16
-	ldi led_state, 0
+	ldi button_flag, 0
 	ret
 
+set_btn_flag:
+	ldi button_flag, 1
+	reti
+
 button_action:
+	ldi r16, 0
+	cp button_flag, r16
+	breq set_btn_flag
+	;ldi button_flag, 0
+	reti
+	
+toggle_led:
+	mov last_button_flag, button_flag
+
 	ldi r16, 'i'
 	call send_char
 	ldi r16, 0
-	cp led_state, r16
+	cp button_flag, r16
 	breq turn_on_4
-	call turn_off_4
-	reti
-	
-	
+	rjmp turn_off_4
+	ret
 
-loop:	rjmp    loop			; stay in infinite loop
+loop:
+	cp button_flag, last_button_flag
+	brne toggle_led
 
-
+	rjmp    loop			; stay in infinite loop
