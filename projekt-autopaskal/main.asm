9fa8571027e9feaa18162282b4fd9c36fb03856f
Lovro Govekar <lovro.govekar@gmail.com>
2021-11-15 17:29:53 +0100

    bruh to je nice sam ne dela

--- projekt-autopaskal/main.asm
+++ projekt-autopaskal/main.asm
@@ -1,96 +1,96 @@
-;
-; projekt-autopaskal.asm
-;
-; Created: 11. 11. 2021 14:30:11
-; Author : Lovro Govekar
-;
+/*
+ * main.asm
+ *
+ *  Created: 14. 11. 2021 19:59:26
+ *   Author: Lovro Govekar
+ */ 
 
-.def led_state = r22
-.def button_flag = r23
-.def last_button_flag = r21
-
-
-.cseg
+ .cseg
 	.org 	0x00
 	rjmp start
-	.org INT0addr
-	rjmp button_action
-	.org 0x34
-	.include "volcini-default.asm"
-	
-    
+	.org 0x034
+	.include "motor.inc"
 
 start:
-	rjmp setup
-
-setup:
-	call setupUART
-	;clr EICRA
-	ldi r16, (1<<ISC01)|(0<<ISC00)	
-	sts EICRA, r16
-	ldi r16, (1<<INT0)				
-	out EIMSK, r16					
-	;ldi r16, (1<<INTF0)
-	;out EIFR, r16
-	sei
-
+	;-----------INITIALIZE STACK POINTER----------------
 	ldi r16, low(RAMEND)
 	out SPL, r16
 	ldi r16, high(RAMEND)
 	out SPH, r16
+	;---------------------------------------------------
 
-	ldi	r16,(1<<PINB4)		; load 00000001 into register 16
-    out     DDRB,r16		; write register 16 to DDRB
-    ; out     PORTB,r16	
+	;-----------SET OUTPUT PINS-------------------------
+	; PB4 - RS display pin
+	ldi	r16,(1<<PINB4)
+    out DDRB,r16
+	out PINB,r16 ; set RS to instruction
 
-	clr	r16		; load 00000001 into register 16
-    out DDRD,r16		; write register 16 to DDRB
-	ldi r16,(1<<PIND2)
-	out PORTD, r16
+	; PD6 - D4 display data pin
+	ldi	r16,(1<<PIND6)
+    out DDRD,r16
 
-	ldi led_state, 0
-	ldi button_flag, 0
-	mov last_button_flag, button_flag
-	;call turn_on_4
+	; PD5 - D5 display data pin
+	ldi	r16,(1<<PIND5)
+    out DDRD,r16
+
+	; PD4 - D6 display data pin
+	ldi	r16,(1<<PIND4)
+    out DDRD,r16
+
+	; PD3 - D7 display data pin
+	ldi	r16,(1<<PIND3)
+    out DDRD,r16
+
+	; PB3 - ENABLE display pin
+	ldi	r16,(1<<PINB3)
+    out DDRB,r16
+	;---------------------------------------------------
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	call delay_1ms
+	; ne tega ni noben vidu ssssssh
+
+	; function set command
+	send_lcd_4bits 0b00001000
+	send_lcd_4bits 0b00100000
+
+	; clear display command
+	;send_lcd_4bits 0b00000000
+	;send_lcd_4bits 0b00001000
+
+	; display on/off command
+	send_lcd_4bits 0b00000000
+	send_lcd_4bits 0b01111000
+
+	; clear display command
+	send_lcd_4bits 0b00000000
+	send_lcd_4bits 0b00001000
 
 	rjmp loop
 
-turn_on_4:
-	ldi	r16,(1<<PINB4)		; load 00000001 into register 16
-	out PORTB,r16		; write register 16 to PORTB
-	ldi button_flag, 1
+delay_1ms:
+	ldi  r18, 21
+	ldi  r19, 199
+	call L1
 	ret
 
-turn_off_4:
-	ldi	r16,(0<<PINB4)		; load xxxxxxx0 into register 16
-	out PORTB, r16
-	ldi button_flag, 0
-	ret
-
-set_btn_flag:
-	ldi button_flag, 1
-	reti
-
-button_action:
-	ldi r16, 0
-	cp button_flag, r16
-	breq set_btn_flag
-	;ldi button_flag, 0
-	reti
-	
-toggle_led:
-	mov last_button_flag, button_flag
-
-	ldi r16, 'i'
-	call send_char
-	ldi r16, 0
-	cp button_flag, r16
-	breq turn_on_4
-	rjmp turn_off_4
+L1: dec  r19
+    brne L1
+    dec  r18
+    brne L1
 	ret
 
 loop:
-	cp button_flag, last_button_flag
-	brne toggle_led
-
-	rjmp    loop			; stay in infinite loop
+	rjmp loop
