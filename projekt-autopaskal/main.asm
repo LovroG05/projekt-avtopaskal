b9da125a86b4a97a42f5bbc10b40c06b52cef66e
Lovro Govekar <lovro.govekar@gmail.com>
2021-11-27 20:03:20 +0100

    js nena vem, ampak to zgleda šlik čeprou ne dela

--- projekt-autopaskal/main.asm
+++ projekt-autopaskal/main.asm
@@ -20,29 +20,29 @@ start:
 	;---------------------------------------------------
 
 	;-----------SET OUTPUT PINS-------------------------
-	; PB4 - RS display pin
-	ldi	r16,(1<<PINB4)
-    out DDRB,r16
-	out PINB,r16 ; set RS to instruction
+	; PD3 - RS display pin
+	ldi	r16,(1<<PIND3)
+    out DDRD,r16
+	out PIND,r16 ; set RS to instruction
 
-	; PD6 - D4 display data pin
-	ldi	r16,(1<<PIND6)
+	; PD4 - D4 display data pin
+	ldi	r16,(1<<PIND4)
     out DDRD,r16
 
 	; PD5 - D5 display data pin
 	ldi	r16,(1<<PIND5)
     out DDRD,r16
 
-	; PD4 - D6 display data pin
-	ldi	r16,(1<<PIND4)
+	; PD6 - D6 display data pin
+	ldi	r16,(1<<PIND6)
     out DDRD,r16
 
-	; PD3 - D7 display data pin
-	ldi	r16,(1<<PIND3)
+	; PD7 - D7 display data pin
+	ldi	r16,(1<<PIND7)
     out DDRD,r16
 
-	; PB3 - ENABLE display pin
-	ldi	r16,(1<<PINB3)
+	; PB0 - ENABLE display pin
+	ldi	r16,(1<<PINB0)
     out DDRB,r16
 	;---------------------------------------------------
 	call delay_1ms
@@ -63,21 +63,12 @@ start:
 	; ne tega ni noben vidu ssssssh
 
 	; function set command
-	send_lcd_4bits 0b00001000
-	send_lcd_4bits 0b00100000
+	send_lcd_4bits 0b0010_1000
+	call delay_1ms
 
-	; clear display command
-	;send_lcd_4bits 0b00000000
-	;send_lcd_4bits 0b00001000
-
-	; display on/off command
-	send_lcd_4bits 0b00000000
-	send_lcd_4bits 0b01111000
-
-	; clear display command
-	send_lcd_4bits 0b00000000
-	send_lcd_4bits 0b00001000
-
+	;display on/off command
+	send_lcd_4bits 0b0000_1111
+	call delay_1ms
 	rjmp loop
 
 delay_1ms:
