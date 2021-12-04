e3687c3a52db4e5906c3d2cc84f127bc4f53e5e3
unknown <lovro.govekar@gmail.com>
2021-12-04 06:43:46 -0800

    RATAL MI JE

--- projekt-autopaskal/main.asm
+++ projekt-autopaskal/main.asm
@@ -8,11 +8,18 @@
  *	DISCLAIMER: ne vem kaj delam, commenti prolly niso accurate
  */ 
 
+/* E = PINB0
+
+ RS = PIND3
+ D4 = PIND4
+ D5 = PIND5
+ D5 = PIND6
+ D7 = PIND7*/
+
  .cseg
 	.org 0x00
 	rjmp start
 	.org 0x034
-	.include "motor.inc"
 
 start:
 	;-----------INITIALIZE STACK POINTER----------------
@@ -23,26 +30,10 @@ start:
 	;---------------------------------------------------
 
 	;-----------SET OUTPUT PINS-------------------------
-	; PD3 - RS display pin
-	ldi	r16,(1<<PIND3)
+	; set DDRD to OUTPUT GOD DAMMNIT
+	ser r16
     out DDRD,r16
-	out PIND,r16 ; set RS to instruction
-
-	; PD4 - D4 display data pin
-	ldi	r16,(1<<PIND4)
-    out DDRD,r16
-
-	; PD5 - D5 display data pin
-	ldi	r16,(1<<PIND5)
-    out DDRD,r16
-
-	; PD6 - D6 display data pin
-	ldi	r16,(1<<PIND6)
-    out DDRD,r16
-
-	; PD7 - D7 display data pin
-	ldi	r16,(1<<PIND7)
-    out DDRD,r16
+	
 
 	; PB0 - ENABLE display pin
 	ldi	r16,(1<<PINB0)
@@ -70,15 +61,27 @@ start:
 	; okej apparently rabm H od func set poslt 2x so here it goes
 	call func_set_H
 	call delay_1ms
-
-	; function set command
-	send_lcd_4bits 0b0010_1000
+	call delay_1ms
+	call delay_1ms
 	call delay_1ms
 	call delay_1ms
 
-	;display on/off command
-	send_lcd_4bits 0b0000_1111
+	ldi r20, 0x28
+	call func_send
 	call delay_1ms
+	
+	ldi r20, 0b0000_1111
+	call func_send
+	call delay_1ms
+
+	ldi r20, 0b0000_0001
+	call func_send
+	call delay_1ms
+
+	ldi r20, 0b0000_0111
+	call func_send
+	call delay_1ms
+
 	rjmp loop
 
 delay_1ms:
@@ -93,15 +96,45 @@ L1: dec  r19
     brne L1
 	ret
 
+toggle_enable_pin:
+	ldi r16,(1<<PINB0)
+	out PORTB, r16
+	nop
+	nop
+	nop
+	ldi r16,(0<<PINB0)
+	out PORTB, r16
+	ret
+
 func_set_H:
-	ldi	r20, 0b0010_1000	; naloži parameter (ukaz za lcd) v r20
+	ldi	r20, 0b0010_0000	; naloži parameter (ukaz za lcd) v r20
 	mov r21, r20		; kopira r20 v r21
 	andi r21, 0b1111_0000	; po?isti L ukaza
 	in r17, PORTD		; naloži PORTD v r17
 	andi r17, 0b0000_1111 ; po?isti zgornje bite
 	or r17, r21			; kombinira r17 in r21
 	out PORTD, r17		; nastavi PORTD na r17
-	toggle_enable_pin
+	call toggle_enable_pin
+	ret
+
+func_send:
+	mov r21, r20
+	andi r21, 0b1111_0000
+	in r17, PORTD
+	andi r17, 0b0000_1111
+	or r17, r21
+	out PORTD, r17
+	call toggle_enable_pin
+
+	mov r21, r20
+	swap r21
+	andi r21, 0b1111_0000
+	in r17, PORTD
+	andi r17, 0b0000_1111
+	or r17, r21
+	out PORTD, r17
+	call toggle_enable_pin
+	ret
 
 loop:
 	rjmp loop
